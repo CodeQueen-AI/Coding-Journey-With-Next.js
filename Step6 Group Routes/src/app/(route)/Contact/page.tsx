f32f02399497f6192f66fd1126beb16101726e30
Sumbal Naz <Codeq209@gmail.com>
2024-12-25 17:49:54 +0500

    Add files via upload

---
+++ Step6 Group Routes/src/app/(route)/Contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-green-800 text-white flex items-center justify-center h-screen text-6xl font-extrabold'>
+      Contact Page!
+    </div>
+  )
+}
+
+export default page
